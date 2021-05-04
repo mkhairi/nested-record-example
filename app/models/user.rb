d4beb25b81fb26d7d1e403ed7f03791742331508
Mohd Khairi Mohd Adnan <khairi@labs.my>
2021-05-04 17:11:05 +0800

    Fix type and change attributes writer to rewrite

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_one_nested :profile
   has_many_nested :emails,
-                  attributes_writter: {
-                    strategy: :upsert,
+                  attributes_writer: {
+                    strategy: :rewrite,
                     reject_if: ->(attributes) { attributes['_destroy'].present? }
                   },
                   primary_key: :email
